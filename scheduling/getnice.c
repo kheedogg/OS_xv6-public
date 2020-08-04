99b1bceb5c31ec0728ddb6a06840c1dba77e37d8
kheedogg <54922374+kheedogg@users.noreply.github.com>
2020-08-05 00:25:29 +0900

    xv6 for scheduling

---
+++ scheduling/getnice.c
@@ -0,0 +1,6 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int getnice(int pid) {
+  if(
---
+++ scheduling/minitop.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+  int pid=0;
+  ps(pid);
+  exit();
+}
