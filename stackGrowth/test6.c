2a02f0e925bdb09d29280e5b0050d067fd61ec49
kheedogg <54922374+kheedogg@users.noreply.github.com>
2020-08-05 00:34:20 +0900

    xv6 for Stack Growth

---
+++ stackGrowth/test6.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "memlayout.h"
+
+int
+main(int argc, char **argv)
+{
+	char *a;
+	int ppid, pid;
+
+	// invalid access over kernel space
+	printf(1, "TEST6: ");
+
+	for(a = (char *)(KERNBASE); a < (char *)(KERNBASE+2000000); a += 50000){
+		ppid = getpid();
+		pid = fork();
+		if(pid == 0){
+			*a = 1;
+			printf(1, "FAIL\n");
+			kill(ppid);
+			exit();
+		}
+		wait();
+	}
+
+	printf(1, "OK\n");
+	exit();
+}
